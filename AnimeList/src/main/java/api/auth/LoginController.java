c590dc016216807e932a41422bab7f0273bf4c09
SadBeatrice <tiaslucena@hotmail.com>
2025-05-10 03:12:28 -0300

    Alterações de liberação para o servidor da VM

--- AnimeList/src/main/java/api/auth/LoginController.java
+++ AnimeList/src/main/java/api/auth/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/login")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("*")
 public class LoginController {
 
 	// Injeta o LoginService
